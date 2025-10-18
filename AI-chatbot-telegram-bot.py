06ad6c2e55e0efaf5290d8a2a6f3d646dbea13ac
Abhisumat Kundu <156107158+AbhisumatK@users.noreply.github.com>
2025-10-18 14:16:10 +0530

    Add files via upload

---
+++ AI-chatbot-telegram-bot.py
@@ -0,0 +1,59 @@
+TOKEN = '8276104249:AAGsdrlEmU7VRfeqFEBKXBWzJJ_BwqHw8-4'
+BOT_USERNAME = '@ai_mini_chatbot'
+AI_API = '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'
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from groq import Groq
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! I am an AI bot. Type /help to see what I can do.')
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('I am an AI bot. Please type something!')
+
+def handle_response(test: str) -> str:
+    client = Groq(api_key=AI_API)
+    response = client.chat.completions.create(
+        model="moonshotai/kimi-k2-instruct-0905",
+        messages=[
+            {"role": "system", "content": "You are a helpful AI assistant. Respond to the user queries accurately and concisely. Do not use any asterisks or special formatting. write in a friendly tone."},
+            {"role": "user", "content": test}
+        ]
+    )
+    return response.choices[0].message.content
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type = update.message.chat.type
+    text = str(update.message.text)
+
+    print(f'User ({update.message.chat.id}) in {message_type} sent: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text = text.replace(BOT_USERNAME, '').strip()
+            response = handle_response(new_text)
+        else:
+            return
+    else:
+        response = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('help', help))
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    app.add_error_handler(error)
+
+    print('Bot is running...')
+    app.run_polling(poll_interval=3)
